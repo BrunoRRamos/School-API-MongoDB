8492b5ee8f30866dd34aca3779a23af3ec9a7b49
MisterBeamCs <103748132+BrunoRRamos@users.noreply.github.com>
2023-03-06 18:44:29 -0300

    fix: refactoring variable

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from "express";
 import db from "./config/dbConnect.js";
-import rourtes from "./routes/index.js";
+import routes from "./routes/index.js";
 
 db.on("error", console.log.bind(console, "Connection Error"));
 db.once("open", () => {
@@ -9,6 +9,6 @@ db.once("open", () => {
 
 const app = express();
 app.use(express.json());
-rourtes(app);
+routes(app);
 
 export default app;
